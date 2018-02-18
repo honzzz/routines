eb89de2e74db2953506191920cc57ecce6ce0de1
Jan Bilek <honzzz@gmail.com>
2018-02-18 10:27:17 +0100

    First working version

---
+++ routine.js
@@ -0,0 +1,225 @@
+// cache elements
+var $name = document.getElementById('name');
+var $note = document.getElementById('note');
+var $save = document.getElementById('save');
+
+// check URL params
+var url_query = window.location.search.substring(1);
+
+// get pack ID
+var routine_id = getQueryVariable(url_query, 'routine');
+
+// is user logged in?
+var $routines = document.getElementById('routines');
+var user = $routines.getAttribute('data-user');
+
+var data;
+var routine = {};
+
+// get URL params
+// based on https://css-tricks.com/snippets/javascript/get-url-variables/
+function getQueryVariable(query, param) {
+    var vars = query.split("&");
+    for (var i=0;i<vars.length;i++) {
+        var pair = vars[i].split("=");
+        if (pair[0] == param) {
+            return pair[1];
+        }
+    }
+    return 0;
+}
+
+// compare first N items of two arrays
+function arraysStartSame(n, array1, array2) {
+    for (var i=0; i<=n; i++) {
+        if (array1[i] != array2[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function saveRoutine() {
+
+    // get name
+    routine.name = $name.value;
+    // get note
+    if ($note.value) {
+        routine.note = $note.value;
+    }
+
+    // check for sub-tasks specified in brackets
+    // example:  routine "strength training" has sub-tasks [biceps, abs, legs]
+    var subroutines = routine.name.match(/\[(.*?)\]/);
+    if (subroutines) {
+        // get array of tasks (map function trims empty spaces)
+        var tasks = subroutines[1].split(',').map(function(item) {return item.trim();});
+
+        // clean empty values from array
+        tasks = tasks.filter(function(n) {return n;});
+
+        // if sub-tasks already exist and items until currently selected item are the same
+        if (routine.sub && arraysStartSame(routine.sub.pos, routine.sub.tasks, tasks)) {
+            // just rewrite tasks and keep position
+            routine.sub.tasks = tasks
+        }
+        else {
+            // create object for storing sub-tasks
+            routine.sub = {
+                'tasks': tasks,
+                'pos': 0 // set starting position
+            };
+        }
+
+        // cut everything in brackets out of name
+        routine.name = routine.name.replace(subroutines[0],"");
+    }
+    else {
+        // if this routine previously had sub-tasks that are now being removed
+        if (routine.tasks) {
+            delete routine.tasks;
+        }
+    }
+
+    // TODO trim spaces around routine name
+    routine.name = routine.name.trim();
+
+
+    // VALIDATE
+
+    // name longer than 2 characters
+    if (routine.name.length < 3) {
+        alert('Routine name should be at least 3 characters long.');
+        return;
+    }
+
+    // days
+    routine.days = routine.days || 0;
+
+    // state (done or not)
+    // if NO type routine
+    if ((routine.name.substring(0, 3)).toLowerCase() === "no ") {
+        // set to true for new NO routine, use state for old routine
+        routine.done = (routine.done == undefined ? true : routine.done);
+        routine.no = true;
+    }
+    else {
+        // set to false for new routine, use state for old routine
+        routine.done = (routine.done == undefined ? false : routine.done);
+    }
+
+    // user logged in
+    if (user) {
+        // store routine in db
+    }
+    else {
+        // if new routine
+        if (routine_id === 'new') {
+            // use timestamp as id
+            routine.id = (new Date()).getTime();
+
+            // push into data (to the beginning of array - newest at the top)
+            data.routines.unshift(routine);
+        }
+        else {
+            // insert old id into routine object to be saved
+            routine.id = routine_id;
+
+            // modify routine in data
+            // loop through routines
+            for (var i=0; i<data.routines.length; i++) {
+                // find the routine to edit
+                if (data.routines[i].id == routine_id) {
+                    // load data into form
+                    data.routines[i] = routine;
+
+                    break;
+                }
+            }
+        }
+
+        // store data locally
+        localStorage.setItem('routines', JSON.stringify(data));
+    }
+
+    // redirect to home page
+    //window.location.href = './index.html'; // index.html is there for android app
+    window.location.href = './';
+}
+
+// old routine being edited
+if (routine_id !== 'new') {
+    // user logged in
+    if (user) {
+        // get routine from server
+    }
+    else {
+        // get all data from local storage
+        data = JSON.parse(localStorage.getItem('routines'));
+
+        // loop through routines
+        for (var i=0; i<data.routines.length; i++) {
+            // find the routine to edit
+            if (data.routines[i].id == routine_id) {
+                // store edited routine in variable
+                routine = data.routines[i];
+
+                // load editable data into form
+                // load description if there is any
+                if (data.routines[i].note) {
+                    $note.value = data.routines[i].note;
+                }
+
+                // if there are subtasks
+                var sub_tasks = '';
+                if (data.routines[i].sub) {
+                    // get them so they can be appended to name
+                    sub_tasks = JSON.stringify(data.routines[i].sub.tasks);
+
+                    // strip quotation marks
+                    sub_tasks = sub_tasks.replace(/"/g,"");
+                }
+
+                // load name
+                $name.value = data.routines[i].name+sub_tasks;
+
+                break;
+            }
+        }
+    }
+}
+// new routine being created
+else {
+    // hide delete button
+
+    // user not logged in
+    if (!user) {
+        // get all data from local storage
+        data = JSON.parse(localStorage.getItem('routines')) || {
+            'last_check': (new Date()).setHours(0,0,0,0), // set last check to today if no data yet (user just started using app)
+            'routines': []
+        };
+    }
+}
+
+
+// keyboard shortcut - saving with enter
+    window.onkeyup = function(e) {
+        var key = e.keyCode || e.keyCode; // TODO zkontrolovat, že funguje v různých prohlížečích
+
+        if (key == 13) { // enter
+            // jump back
+            $save.click();
+        }
+    };
+
+
+$save.addEventListener('click', saveRoutine, false);
+
+
+// remove #_=_ artefact left after facebook login
+if (window.location.hash == '#_=_'){
+    history.replaceState 
+        ? history.replaceState(null, null, window.location.href.split('#')[0])
+        : window.location.hash = '';
+}
---
+++ routines.js
@@ -0,0 +1,317 @@
+var day_in_ms = 86400000; // day in milliseconds
+
+var $routines = document.getElementById('routines');
+var user = $routines.getAttribute('data-user');
+var data;
+
+// this cycles through array in infinite loop, when it gets to last item it continues from first
+// function returns index of target item
+function cycleThroughArray(starting_position, steps, array_length) {
+    var result = starting_position;
+
+    for (var i = 0; i < steps; i++) {
+        result = (result+1 < array_length) ? result+1 : 0;
+    }
+
+    return result;
+}
+
+// new day
+function checkNewDay() {
+    if (!data) {return;}
+
+    //var today = (new Date()).setHours(0,0,0,0);console.log(today);
+    var today = 1518908400000+(0*day_in_ms);
+
+    // if another day
+    if (data.last_check !== today) {console.log('new day');
+        // loop through all routines
+         for (var i=0; i < data.routines.length; i++) {
+            // if NO type routine
+            if (data.routines[i].no) {
+                // add days since last check
+                var days_to_add = (today-data.last_check)/day_in_ms;console.log(data.routines[i].done);
+
+                // if not done the last check
+                if (!data.routines[i].done) {
+                    // deduct one day
+                    days_to_add = days_to_add-1;
+                }
+
+                // adjust number of days
+                data.routines[i].days = data.routines[i].days+days_to_add;console.log(data.routines[i].days,days_to_add);
+
+                // if there are sub-tasks
+                if (data.routines[i].sub) {
+                    // go to next task
+                    data.routines[i].sub.pos = cycleThroughArray(data.routines[i].sub.pos, days_to_add, data.routines[i].sub.tasks.length);
+                }
+
+                // reset state to 'done'
+                data.routines[i].done = true; // this has to be done after state from day of last check checked
+            }
+            else {
+                // if last check was not yesterday or it was not done
+                if (data.last_check !== (today - day_in_ms) || !data.routines[i].done) {
+                    // reset day count
+                    data.routines[i].days = 0;
+                }
+                // if last check was yesterday and it was done
+                else {
+                    // check if there are sub-tasks...
+                    if (data.routines[i].sub) {
+                        // ...and go to next task
+                        data.routines[i].sub.pos = cycleThroughArray(data.routines[i].sub.pos, 1, data.routines[i].sub.tasks.length);
+                    }
+                }
+
+                // reset state to 'not done'
+                data.routines[i].done = false;
+            }
+        }
+
+        // re-draw view
+        renderRoutines(data.routines);
+
+        // update date
+        data.last_check = today;
+
+        // store updated date
+        if (user) {
+            // store in db
+        }
+        else {
+            localStorage.setItem('routines', JSON.stringify(data));
+        }
+    }
+}
+
+function moveDataToDB () {
+
+    // save marker that data moved to db
+    localStorage.setItem('routines', JSON.stringify({'moved': 1}));
+
+}
+
+function renderRoutines(routines) {
+    var html = '', icon = '', fill = '', path = '', state_title = '';
+    var checkmark = 'M1412 734q0-28-18-46l-91-90q-19-19-45-19t-45 19l-408 407-226-226q-19-19-45-19t-45 19l-91 90q-18 18-18 46 0 27 18 45l362 362q19 19 45 19 27 0 46-19l543-543q18-18 18-45zm252 162q0 209-103 385.5t-279.5 279.5-385.5 103-385.5-103-279.5-279.5-103-385.5 103-385.5 279.5-279.5 385.5-103 385.5 103 279.5 279.5 103 385.5z';
+    var frownyface = 'M1262 1229q8 25-4 48.5t-37 31.5-49-4-32-38q-25-80-92.5-129.5t-151.5-49.5-151.5 49.5-92.5 129.5q-8 26-31.5 38t-48.5 4q-26-8-38-31.5t-4-48.5q37-121 138-195t228-74 228 74 138 195zm-494-589q0 53-37.5 90.5t-90.5 37.5-90.5-37.5-37.5-90.5 37.5-90.5 90.5-37.5 90.5 37.5 37.5 90.5zm512 0q0 53-37.5 90.5t-90.5 37.5-90.5-37.5-37.5-90.5 37.5-90.5 90.5-37.5 90.5 37.5 37.5 90.5zm256 256q0-130-51-248.5t-136.5-204-204-136.5-248.5-51-248.5 51-204 136.5-136.5 204-51 248.5 51 248.5 136.5 204 204 136.5 248.5 51 248.5-51 204-136.5 136.5-204 51-248.5zm128 0q0 209-103 385.5t-279.5 279.5-385.5 103-385.5-103-279.5-279.5-103-385.5 103-385.5 279.5-279.5 385.5-103 385.5 103 279.5 279.5 103 385.5z';
+
+    for (var i=0; i < routines.length; i++) {
+        // any routine done
+        if (routines[i].done) {
+            // green check
+            fill = '#67c013';
+            path = checkmark;
+            state_title = ' Marked as done ';
+        }
+        else {
+            // if NO routine not done
+            if (routines[i].no) {
+                // gray frowny face
+                fill = '#ea0000';
+                path = frownyface;
+                state_title = ' The chain was broken but tomorrow is another day ';
+            }
+            else {
+                // if normal routine not done
+                fill = '#eaeaea';
+                path = checkmark;
+                state_title = ' Click to mark as done ';
+            }
+        }
+
+        // if there are sub-tasks
+        var tasks = '';
+        if (routines[i].sub) {
+            tasks = ': '+'<span class="task">'+routines[i].sub.tasks[routines[i].sub.pos]+' <span data-id="'+routines[i].id+'" class="skip" title=" Skip to the next sub-task ">&#9656;</span></span>';
+        }
+
+        html = html+'<div class="routine">'+
+        '<svg data-id="'+routines[i].id+'" class="state" fill="'+fill+'" width="28" height="28" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="'+path+'"/>'+
+        '<title>'+state_title+'</title></svg>'+
+        '<a href="routine.html?routine='+routines[i].id+'" class="name" title=" View/edit routine details ">'+routines[i].name+'</a>'+tasks+'<div class="days">'+routines[i].days+'</div></div>';
+    }
+
+    $routines.innerHTML = html;
+
+    // grab all .state elements and attach click listeners
+    var states = document.getElementsByClassName("state");
+    for (var i = 0; i < states.length; i++) {
+        states[i].addEventListener('click', toggleState, false);
+    }
+
+    // grab all .skip elements and attach click listeners
+    var skips = document.getElementsByClassName("skip");
+    for (var i = 0; i < skips.length; i++) {
+        skips[i].addEventListener('click', skipForward, false);
+    }
+}
+
+function toggleIcon(icon, state) {
+    icon.style.fill = (state ? '#6fe600' : '#eaeaea');
+}
+
+// go to next sub-task 
+function skipForward() {
+    // get ID of pack to be skipped
+    var routine_to_skip = this.getAttribute("data-id");
+
+    if (user) {
+
+    }
+    else {
+        // loop through routines
+        for (var i=0; i<data.routines.length; i++) {
+            // find the routine to edit
+            if (data.routines[i].id == routine_to_skip) {
+
+                // jump one sub-task forward
+                data.routines[i].sub.pos = cycleThroughArray(data.routines[i].sub.pos, 1, data.routines[i].sub.tasks.length);
+
+                // re-draw view
+                renderRoutines(data.routines);
+
+                // store data
+                localStorage.setItem('routines', JSON.stringify(data));
+
+                break;
+            }
+        }
+    }
+}
+
+function toggleState() {
+    // get ID of pack to be toggled
+    var routine_to_toggle = this.getAttribute("data-id");
+
+    if (user) {
+
+    }
+    else {
+        // loop through routines
+        for (var i=0; i<data.routines.length; i++) {
+            // find the routine to edit
+            if (data.routines[i].id == routine_to_toggle) {
+
+                // handle state and update days
+                // if NO routine 
+                if (data.routines[i].no) {
+                    // No routine can only by changed to 'not done'
+                    if (data.routines[i].done) {
+                        // if chain longer than 10 days
+                        if (data.routines[i].days > 10) {
+                            // throw a warning
+                            if (confirm('Did you break the chain of '+data.routines[i].days+' days?')) {
+                                data.routines[i].done = 0;
+                                data.routines[i].days = 0;
+                            }
+                        }
+                        else {
+                            data.routines[i].done = 0;
+                            data.routines[i].days = 0;
+                        }
+                    }
+                    else {
+                        // TODO - implement undo if clicked by mistake?
+                        alert('You cannot un-break what has been broken.');
+                    }
+                }
+                // if normal routine
+                else {
+                    // change state to opposite state
+                    data.routines[i].done = !data.routines[i].done;
+
+                    // if new state is 'done'
+                    if (data.routines[i].done) {
+                        // add one day
+                        data.routines[i].days = data.routines[i].days + 1;
+                    }
+                    else {
+                        // deduct one day 
+                        data.routines[i].days = data.routines[i].days - 1;
+                    }
+                }
+
+                // save change
+                localStorage.setItem('routines', JSON.stringify(data));
+
+                // // change color of shuffle icon
+                // toggleIcon(this, data.routines[i].done);
+
+                // re-draw view
+                renderRoutines(data.routines); // TODO is it good to re-draw the whole view when anything changes?
+
+                break;
+            }
+        }
+    }
+}
+
+// function msTranslate(from, to, text) {
+//     // ajax request based on https://stackoverflow.com/a/24468752/716001
+//     var xhr = new XMLHttpRequest();
+//     xhr.open("POST", "/mstranslator", true);
+//     xhr.setRequestHeader("Content-type", "application/json");
+//     xhr.onreadystatechange = function () {
+//         if (xhr.readyState === 4 && xhr.status === 200) {
+//             var response = JSON.parse(xhr.responseText);
+//             $translatedword.textContent = response.translation;
+//         }
+//         else {
+//             $translatedword.textContent = 'ERROR';
+//         }
+//     };
+//     var data = JSON.stringify({
+//         from: from,
+//         to: to,
+//         text: text
+//     });
+//     xhr.send(data);
+// }
+
+
+// LOAD DATA
+
+// if user logged in
+if (user) {
+    // get data from server
+
+    // render data
+    renderRoutines(data.routines);
+}
+else {
+    // try to get data from local storage
+    data = JSON.parse(localStorage.getItem('routines'));
+
+    if (data) {
+        // logged in before, data moved to db
+        if (data.moved) {
+            // remind user to log in
+        }
+        else {
+            // never logged in, uses app, data stored locally
+            renderRoutines(data.routines);
+        }
+    }
+    else {
+        // TODO load explainer
+        $routines.innerHTML = '<p>Explainer will be here...</p>';
+    }
+}
+
+
+// new day?
+checkNewDay(); // check on load
+setTimeout(function() {
+    checkNewDay();
+}, 60000); // check each minute
+
+
+// remove #_=_ artefact left after facebook login
+if (window.location.hash == '#_=_'){
+    history.replaceState 
+        ? history.replaceState(null, null, window.location.href.split('#')[0])
+        : window.location.hash = '';
+}
