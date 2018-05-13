549d27ab967ab3fb423d9d1b8d2ef912101a9990
Jan Bilek <honzzz@gmail.com>
2018-05-13 14:55:07 +0200

    Restoring deleted data - fixed

--- routines.js
+++ routines.js
@@ -311,7 +311,7 @@ if (user) {
 else {
     // try to get data from local storage
     data = JSON.parse(localStorage.getItem('routines'));
-    data = {"last_check":'2018-05-13',"routines":[{"name":"No sweets","note":"Ne buchty, čokolády, oplatky, medovníky atp. Smím buchty od mamky, darované sladkosti, dobroty na svatbách, když pozvu dámu do cukrárny a podobně. ","days":77,"done":true,"no":true,"id":1519593308258},{"name":"No P","note":"No porn. ","days":47,"done":0,"no":true,"id":1519412198345}]}
+    data = {"last_check":'2018-05-13',"routines":[{"name":"No sweets","note":"Ne buchty, čokolády, oplatky, medovníky atp. Smím buchty od mamky, darované sladkosti, dobroty na svatbách, když pozvu dámu do cukrárny a podobně. ","days":77,"done":true,"no":true,"id":1519593308258},{"name":"No P","note":"No porn. ","days":47,"done":true,"no":true,"id":1519412198345}]}
 
     if (data) {
         // logged in before, data moved to db
